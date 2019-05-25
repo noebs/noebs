30f3af5976f07117339c16219aeae3b843015484
Mohamed Yousif <mmbusif@gmail.com>
2019-05-25 14:49:53 +0200

    working with GetAllTransactions

--- dashboard/api.go
+++ dashboard/api.go
@@ -188,7 +188,7 @@ func GetAll(c *gin.Context) {
 	var tran []Transaction
 
 	// another good alternative
-	db.Offset(offset).Find(&tran)
+	db.Where("id >= ?", offset).Find(&tran)
 
 	previous := page - 1
 	next := page + 1
