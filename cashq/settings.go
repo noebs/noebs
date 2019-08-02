3d87116cf241be4c6bd4873b4fab9ad3a000b75d
Mohamed Yousif <mmbusif@gmail.com>
2019-08-02 18:35:49 +0200

    fixing jetbrain not working git client issues

---
+++ cashq/settings.go
@@ -0,0 +1,20 @@
+package main
+
+const (
+	EBSIp = "https://172.16.199.1:8877/QAEBSGateway/"
+)
+
+
+const (
+	ConsumerIsAliveEndpoint = "isAlive"
+	ConsumerPublicKeyEndpoint = "getPublicKey"
+	ConsumerBalanceEndpoint = "getBalance"
+	ConsumerBillInquiryEndpoint = "getBill"
+	ConsumerBillPaymentEndpoint = "payment"
+	ConsumerCardTransferEndpoint = "doCardTransfer"
+	ConsumerPayeesListEndpoint = "getPayeesList"
+	ConsumerChangeIPinEndpoint = "changeIPin"
+	ConsumerPurchaseEndpoint = "specialPayment"
+	ConsumerTransactionStatusEndpoint = "getTransactionStatus"
+
+)
