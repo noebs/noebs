1d4d2a22224d245dd64013e5c0621ef71b0a1bc3
Mohamed Yousif <mmbusif@gmail.com>
2019-08-07 20:28:58 +0200

    adding AmountFields to ConsumerBillersFields. Why would someone send tranAmount when they are inquiring about their amount! fuck EBS

--- ebs_service.go
+++ ebs_service.go
@@ -1472,6 +1472,86 @@ func ConsumerCardTransfer(c *gin.Context) {
 	}
 }
 
+func ConsumerIPinChange(c *gin.Context) {
+	url := EBSIp + ConsumerCardTransferEndpoint // EBS simulator endpoint url goes here.
+	//FIXME instead of hardcoding it here, maybe offer it in the some struct that handles everything about the application configurations.
+	// consume the request here and pass it over onto the EBS.
+	// marshal the request
+	// fuck. This shouldn't be here at all.
+
+	db := database("sqlite3", "test.db")
+	defer db.Close()
+
+	// redis instance
+	redisClient := getRedis()
+
+	var fields = ebs_fields.ConsumerCardTransferFields{}
+
+	bindingErr := c.ShouldBindBodyWith(&fields, binding.JSON)
+
+	switch bindingErr := bindingErr.(type) {
+
+	case validator.ValidationErrors:
+		var details []ErrDetails
+
+		for _, err := range bindingErr {
+
+			details = append(details, ErrorToString(err))
+		}
+
+		payload := ErrorDetails{Details: details, Code: http.StatusBadRequest, Message: "Request fields validation error", Status: BadRequest}
+
+		c.JSON(http.StatusBadRequest, ErrorResponse{payload})
+
+	case nil:
+
+		jsonBuffer, err := json.Marshal(fields)
+		if err != nil {
+			// there's an error in parsing the struct. Server error.
+			er := ErrorDetails{Details: nil, Code: 400, Message: "Unable to parse the request", Status: ParsingError}
+			c.AbortWithStatusJSON(400, ErrorResponse{er})
+		}
+
+		// the only part left is fixing EBS errors. Formalizing them per se.
+		code, res, ebsErr := EBSHttpClient(url, jsonBuffer)
+		log.Printf("response is: %d, %+v, %v", code, res, ebsErr)
+
+		// mask the pan
+		res.MaskPAN()
+
+		transaction := dashboard.Transaction{
+			GenericEBSResponseFields: res.GenericEBSResponseFields,
+		}
+
+		transaction.EBSServiceName = PurchaseTransaction
+
+		if err := db.Table("transactions").Create(&transaction).Error; err != nil {
+			logrus.WithFields(logrus.Fields{
+				"error":   "unable to migrate purchase model",
+				"message": err.Error(),
+			}).Info("error in migrating purchase model")
+		}
+
+		// Write the request onto Redis
+		username := c.GetString("username")
+		if username == "" {
+			username = "invalid_key"
+		}
+
+		redisClient.LPush(username, string(jsonBuffer))
+
+		if ebsErr != nil {
+			payload := ErrorDetails{Code: res.ResponseCode, Status: EBSError, Details: res, Message: EBSError}
+			c.JSON(code, payload)
+		} else {
+			c.JSON(code, gin.H{"ebs_response": res})
+
+		}
+
+	default:
+		c.AbortWithStatusJSON(400, gin.H{"error": bindingErr.Error()})
+	}
+}
 func ConsumerStatus(c *gin.Context) {
 	url := EBSIp + ConsumerStatusEndpoint // EBS simulator endpoint url goes here.
 	//FIXME instead of hardcoding it here, maybe offer it in the some struct that handles everything about the application configurations.
