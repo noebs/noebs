a3d205f313324c2b4076c9a593d9340992ab1d5f
Mohamed Yousif <mmbusif@gmail.com>
2019-10-18 23:08:53 +0200

    making ebs ips sane

--- ebs_fields/ebs_urls.go
+++ ebs_fields/ebs_urls.go
@@ -26,8 +26,8 @@ const (
 	ChangePINEndpoint                = "changePin"
 )
 
-const EBSMerchantIP = "https://172.16.199.1:8181/QAEBSGateway/"
-const EBSMerchantIPLive = "https://172.16.198.14:8888/EBSGateway/"
+const EBSMerchantIPQA = "https://172.16.199.1:8181/QAEBSGateway/"
+const EBSMerchantIP = "https://172.16.198.14:8888/EBSGateway/"
 
 const (
 	PurchaseTransaction             = "PurchaseTransaction"
@@ -47,8 +47,8 @@ const (
 )
 
 const (
-	EBSIp = "https://172.16.199.1:8877/QAConsumer/"
-	EBSIpTesting = "https://172.24.16.30:8443/Consumer/"
+	EBSIpQA = "https://172.16.199.1:8877/QAConsumer/"
+	EBSIp = "https://172.24.16.30:8443/Consumer/"
 )
 
 const (
