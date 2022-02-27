9507dfd94b81023dbfe0691963913aa9327b64dc
Mohamed Yousiff <mmbusif@gmail.com>
2022-02-27 23:57:51 +0200

    adding ,string to tranDateTime field

--- ebs_fields/fields.go
+++ ebs_fields/fields.go
@@ -260,7 +260,7 @@ type GenericEBSResponseFields struct {
 	MiniStatementRecords MinistatementDB `json:"miniStatementRecords,omitempty" gorm:"type:text[]"` //make this gorm-able
 	DisputeRRN           string          `json:"DisputeRRN,omitempty"`
 	AdditionalData       string          `json:"additionalData,omitempty"`
-	TranDateTime         string          `json:"tranDateTime,string"`
+	TranDateTime         int64           `json:"tranDateTime,string"`
 	TranFee              *float32        `json:"tranFee,omitempty"`
 
 	AdditionalAmount *float32 `json:"additionalAmount,omitempty"`
