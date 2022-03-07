99257c1fd152d20d98a10e41d5be89a7130be024
Mohamed Yousiff <mmbusif@gmail.com>
2022-03-07 10:17:36 +0200

    Stronger password validations

---
+++ consumer/helper_test.go
@@ -0,0 +1,30 @@
+package consumer
+
+import "testing"
+
+func Test_validatePassword(t *testing.T) {
+	type args struct {
+		password string
+	}
+	tests := []struct {
+		name string
+		args args
+		want bool
+	}{
+		{"regular_password", args{"12345678"}, false},
+		{"s dollar", args{"MY$SuperPassword11"}, true},
+		{"asterisk", args{"MY*SuperPassword11"}, false},
+		{"plus", args{"MY+SuperPassword11"}, true},
+		{"minus", args{"MY-SuperPassword11"}, false},
+		{"=", args{"MY=SuperPassword11"}, true},
+		{"<", args{"MY>SuperPassword11"}, true},
+		{"&", args{"MY&SuperPassword11"}, false},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := validatePassword(tt.args.password); got != tt.want {
+				t.Errorf("validatePassword() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
