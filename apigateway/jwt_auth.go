fffeafa128f0a2caaafe3feab7d2eba55efd2ee5
Mohamed Yousif <mmbusif@gmail.com>
2022-07-27 10:20:31 +0200

    make a shorter token timeout for test

--- apigateway/jwt_auth.go
+++ apigateway/jwt_auth.go
@@ -28,7 +28,7 @@ func (j *JWTAuth) Init() {
 func (j *JWTAuth) GenerateJWT(serviceID string) (string, error) {
 	// Create a new token object, specifying signing method and the claims
 	// you would like it to contain.
-	expiresAt := time.Now().Add(1 * time.Hour).UTC().Unix()
+	expiresAt := time.Now().Add(1 * time.Minute).UTC().Unix()
 
 	claims := TokenClaims{
 		serviceID,
