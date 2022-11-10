bcdf3b006232ded048d476da7212348661c1d078
Mohamed Yousif <mmbusif@gmail.com>
2022-11-10 19:07:34 +0000

    fix small debug issue

--- config.go
+++ config.go
@@ -235,8 +235,7 @@ func init() {
 	firebaseApp, err := getFirebase()
 	// gorm debug-level logger
 	database.Logger.LogMode(logger.Info)
-	database.Debug().AutoMigrate(&ebs_fields.User{})
-	if err := database.AutoMigrate(&ebs_fields.Card{}, &ebs_fields.EBSResponse{}, &ebs_fields.PaymentToken{}, &ebs_fields.CacheBillers{}, &ebs_fields.CacheCards{}, &ebs_fields.Beneficiary{}); err != nil {
+	if err := database.AutoMigrate(&ebs_fields.User{}, &ebs_fields.Card{}, &ebs_fields.EBSResponse{}, &ebs_fields.PaymentToken{}, &ebs_fields.CacheBillers{}, &ebs_fields.CacheCards{}, &ebs_fields.Beneficiary{}); err != nil {
 		logrusLogger.Fatalf("error in migration: %v", err)
 	}
 	auth = gateway.JWTAuth{NoebsConfig: noebsConfig}
