59be223b8b95c74c1f5684d5f20a96e6d1105132
Mohamed Yousif <mmbusif@gmail.com>
2022-12-08 13:20:26 +0000

    small changes for migration bugs

--- ebs_fields/fields.go
+++ ebs_fields/fields.go
@@ -248,6 +248,7 @@ type EBSResponse struct {
 	SystemTraceAuditNumber int     `json:"systemTraceAuditNumber,omitempty"`
 	ClientID               string  `json:"clientId,omitempty"`
 	PAN                    string  `json:"PAN,omitempty"`
+	PushUUID string `gorm:"uniqueIndex;not null"`
 	ServiceID              string  `json:"serviceId,omitempty"`
 	TranAmount             float32 `json:"tranAmount,omitempty"`
 	PhoneNumber            string  `json:"phoneNumber,omitempty"`
@@ -263,7 +264,7 @@ type EBSResponse struct {
 	PayeeID                string  `json:"payeeId,omitempty"`
 	// Consumer fields
 	PubKeyValue     string `json:"pubKeyValue,omitempty" form:"pubKeyValue"`
-	UUID            string `json:"UUID,omitempty" form:"UUID" gorm:"primaryKey;not null;index:transaction_uuid_index_unique"`
+	UUID            string `json:"UUID,omitempty" form:"UUID" gorm:"primaryKey;not null;"`
 	ResponseMessage string `json:"responseMessage,omitempty"`
 	ResponseStatus  string `json:"responseStatus,omitempty"`
 	ResponseCode    int    `json:"responseCode"`
