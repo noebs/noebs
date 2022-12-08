771f08cac7a517d5f1c57970f4ee693134a5e878
Mohamed Yousif <mmbusif@gmail.com>
2022-12-08 12:35:05 +0000

    explicit migration in config.go

--- config.go
+++ config.go
@@ -246,9 +246,19 @@ func init() {
 	firebaseApp, err := getFirebase()
 	// gorm debug-level logger
 	database.Logger.LogMode(logger.Info)
-	if err := database.Debug().AutoMigrate(&consumer.PushData{}, &ebs_fields.User{}, &ebs_fields.Card{}, &ebs_fields.EBSResponse{}, &ebs_fields.Token{}, &ebs_fields.CacheBillers{}, &ebs_fields.CacheCards{}, &ebs_fields.Beneficiary{}); err != nil {
+
+	// check database foreign key for user & credit_cards exists or not
+	database.Migrator().DropConstraint(&consumer.PushData{}, "Transactions")
+	database.Migrator().DropConstraint(&consumer.PushData{}, "fk_push_data_ebs_data")
+	if err := database.Debug().AutoMigrate(&consumer.PushData{}, &ebs_fields.User{},
+		 &ebs_fields.Card{}, &ebs_fields.EBSResponse{}, &ebs_fields.Token{},
+		  &ebs_fields.CacheBillers{}, &ebs_fields.CacheCards{}, &ebs_fields.Beneficiary{}); err != nil {
 		logrusLogger.Fatalf("error in migration: %v", err)
 	}
+	// check database foreign key for user & credit_cards exists or not
+	database.Migrator().HasConstraint(&consumer.PushData{}, "Transactions")
+	database.Migrator().HasConstraint(&consumer.PushData{}, "fk_push_data_ebs_data")
+	
 	auth = gateway.JWTAuth{NoebsConfig: noebsConfig}
 
 	auth.Init()
